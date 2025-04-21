10b49d36e01fca6ac1829e6aa945595cf0499d89
Chris Bailey <baileybunch5dad@gmail.com>
2025-04-21 14:52:07 -0400

    merge results across threads

--- krm_capp.cpp
+++ krm_capp.cpp
@@ -13,147 +13,158 @@ ABSL_FLAG(std::string, target, "localhost:50051", "Server address");
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using handshake::HandShake;
-using handshake::ArrayRequest;
+using handshake::StackedTable;
 using handshake::ArrayResponse;
+using handshake::AggregateRequest;
+using handshake::AggregateResponse;
+using handshake::HandShake;
 
+class HandShakeClient
+{
+public:
+    HandShakeClient(std::shared_ptr<Channel> channel)
+        : stub_(HandShake::NewStub(channel)) {}
 
-class HandShakeClient {
- public:
-  HandShakeClient(std::shared_ptr<Channel> channel)
-      : stub_(HandShake::NewStub(channel)) {}
+    std::string SendArray(const std::string &dnames, const std::vector<double> &dvals,
+                          const std::string &snames, const std::vector<std::string> &svals)
+    {
+        StackedTable request;
+        request.set_doublename(dnames);
+        for (auto dval : dvals)
+        {
+            request.add_doublevalues(static_cast<double>(dval)); // Example: i / 10.0
+        }
+        request.set_stringname(snames);
+        for (auto sval : svals)
+        {
+            request.add_stringvalues(sval); // Example: i / 10.0
+        }
 
-  
-  std::string SendArray(const std::string& dnames, const std::vector<double>& dvals,
-  const std::string& snames, const std::vector<std::string>& svals) {
-    ArrayRequest request;
-    request.set_doublename(dnames);
-    for (auto dval : dvals) {
-        request.add_doublevalues(static_cast<double>(dval)); // Example: i / 10.0
+        // Call the SendArray RPC
+        ArrayResponse response;
+        ClientContext context;
+        Status status = stub_->SendTable(&context, request, &response);
+
+        // Handle the response
+        if (status.ok())
+        {
+            return response.message();
+        }
+        else
+        {
+            std::cout << status.error_code() << ": " << status.error_message()
+                      << std::endl;
+            return "RPC failed";
+            
+        }
     }
-    request.set_stringname(snames);
-    for (auto sval : svals) {
-        request.add_stringvalues(sval); // Example: i / 10.0
+
+    int Aggregate(const std::vector<int> &ports)
+    {
+        AggregateRequest request;
+        for (auto p : ports)
+        {
+            request.add_ports(p);
+        }
+
+        //Call the Aggregate RPC
+        AggregateResponse response;
+        ClientContext context;
+        Status status = stub_->Aggregate(&context, request, &response);
+
+        // Handle the response
+        if (status.ok())
+        {
+            return response.return_code();
+        }
+        else
+        {
+            std::cout << status.error_code() << ": " << status.error_message()
+                      << std::endl;
+            return (-1);
+        }
     }
 
-    // Call the SendArray RPC
-    ArrayResponse response;
-    ClientContext context;
-    Status status = stub_->SendArray(&context, request, &response);
-
-    // Handle the response
-    if (status.ok()) {
-      return response.message();
-    } else {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return "RPC failed";
-    }    
-  }
-
- private:
-  std::unique_ptr<HandShake::Stub> stub_;
+private:
+    std::unique_ptr<HandShake::Stub> stub_;
 };
 
-
 // Function to run a client in a separate thread
-void AsynchStreamDataToPython(HandShakeClient& handshakeClient) {
-    for(int i=0;i<10;i++) {
-            // Generate 5000 random doubles
+void AsynchStreamDataToPython(HandShakeClient *handshakeClient)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        // Generate 5000 random doubles
         std::vector<double> doubles;
         doubles.reserve(5000);
-        
+
         // Use random number generator for doubles between 0.0 and 100.0
         std::random_device rd;
         std::mt19937 gen(rd());
         std::uniform_real_distribution<> dis(0.0, 100.0);
-        
+
         std::cout << "Generating 5000 random doubles..." << std::endl;
-        for (int i = 0; i < 5000; i++) {
-            doubles.push_back(dis(gen));
+        for (int i = 0; i < 5000; i++)
+        {
+            double dd = dis(gen);
+            // if(i < 2000)
+            //     dd = std::floor(dd);
+            doubles.push_back(dd);
         }
 
-    std::string dname = "DoubleHeader";
-    std::string sname = "StringHeader";
+        std::string dname = "mortage loan payment interest gain";
+        std::string sname = "txn_id scenario product_id load_id run_id";
         std::cout << "Generating 5000 strings..." << std::endl;
         std::vector<std::string> strings;
         strings.reserve(5000);
-        for(int i=0;i<5000;i++) {
+        for (int i = 0; i < 5000; i++)
+        {
             strings.push_back(std::string("Number ") + std::to_string(i));
         }
 
+        std::string reply = handshakeClient->SendArray(dname, doubles, sname, strings);
 
-    std::string reply = handshakeClient.SendArray(dname,doubles,sname,strings);
-
-    std::cout << "Client returned: " << reply << std::endl;
+        std::cout << "Client returned: " << reply << std::endl;
     }
 }
 
-void RunClient(const std::string& server_address, const std::string& client_name) {
+// void RunClient(const std::string& server_address, const std::string& client_name) {
+void RunClient(HandShakeClient *client, const std::string &client_name)
+{
     // Create a gRPC client and connect to the given server address
-    HandShakeClient client(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
+    // HandShakeClient client(grpc::CreateChannel(server_address, grpc::InsecureChannelCredentials()));
     AsynchStreamDataToPython(client);
 }
 
-int main() {
+int main()
+{
     std::cout << "Starting multi-threaded C++ krm_capp talking to multiprocess krm_pyapp" << std::endl;
     // List of server addresses
-    std::vector<std::string> server_addresses = {"localhost:50051", "localhost:50052"};
-    
+    std::vector<int> ports = { 50051, 50052, 50053, 50054};
+    std::vector<std::string> server_addresses;
+    for(auto p : ports)
+        server_addresses.emplace_back("localhost:" + std::to_string(p));
+    std::vector<HandShakeClient *> clients;
     // Vector to hold threads
     std::vector<std::thread> threads;
 
     // Launch threads for each server
-    for (size_t i = 0; i < server_addresses.size(); ++i) {
-        threads.emplace_back(RunClient, server_addresses[i], "Client" + std::to_string(i + 1));
+    for (size_t i = 0; i < server_addresses.size(); ++i)
+    {
+        HandShakeClient *client = new HandShakeClient(grpc::CreateChannel(server_addresses[i], grpc::InsecureChannelCredentials()));
+        clients.emplace_back(client);
+        threads.emplace_back(RunClient, client, "Client" + std::to_string(i + 1));
     }
 
     // Join all threads
-    for (auto& thread : threads) {
+    for (auto &thread : threads)
+    {
         thread.join();
     }
-    std::cout << "Everything worked" << std::endl;
+    std::cout << "Everything worked multi-threaded, now aggregate with threads" << std::endl;
+    clients[0]->Aggregate(ports);
+    std::cout << "Thread aggregation finished" << std::endl;
+
+    return 0;
 }
 
-
-// int main() {
-//     HandShakeClient py1 = HandShakeClient(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
-//     std::thread t1(AsynchStreamDataToPython,std::ref(py1));
-//     HandShakeClient py2 = HandShakeClient(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
-//     std::thread t2(AsynchStreamDataToPython,std::ref(py2));
-//     t1.join();
-//     t2.join();
-//     return 0;
-// }
-
-
-// int main(int argc, char** argv) {
-//   absl::ParseCommandLine(argc, argv);
-//   // Instantiate the client. It requires a channel, out of which the actual RPCs
-//   // are created. This channel models a connection to an endpoint specified by
-//   // the argument "--target=" which is the only expected argument.
-//   std::string target_str = absl::GetFlag(FLAGS_target);
-//   // We indicate that the channel isn't authenticated (use of
-//   // InsecureChannelCredentials()).
-//   HandShakeClient handshakeClient(
-//       grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
-
-//       // Generate 5000 random doubles
-//     std::vector<double> doubles;
-//     doubles.reserve(5000);
-    
-//     // Use random number generator for doubles between 0.0 and 100.0
-//     std::random_device rd;
-//     std::mt19937 gen(rd());
-//     std::uniform_real_distribution<> dis(0.0, 100.0);
-    
-//     std::cout << "Generating 5000 random doubles..." << std::endl;
-//     for (int i = 0; i < 5000; i++) {
-//         doubles.push_back(dis(gen));
-//     }
-
-
-//   std::string reply = handshakeClient.SendArray(doubles);
-//   std::cout << "Greeter received: " << reply << std::endl;
-//   return 0;
-// }
--- krm_pyapp.py
+++ krm_pyapp.py
@@ -23,22 +23,66 @@ import pandas as pd
 import numpy as np
 import multiprocessing
 
-ports = [50051,50052]
+ports = [50051,50052,50053,50054]
 
 class HandShakeServer(handshake_pb2_grpc.HandShake):
-    def SendArray(self, request, context):
+    def SendTable(self, request, context):
         dname: str = request.doublename
         dvals: np.array = np.array(request.doublevalues, dtype=np.float64)
         sname: str = request.stringname
-        svals: np.array = np.array(request.stringvalues, dtype=np.string_)
-        df = pd.DataFrame({dname: dvals, sname: svals})
+        svals: np.array = np.array(request.stringvalues, dtype=str)
+        dnametoks  = dname.split(' ')
+        nds = len(dnametoks)
+        nrows = len(dvals) / nds
+        dc = {}
+        for i in range(nds):
+            colname = dnametoks[i]
+            startidx = int(i*nrows)
+            endidx = int((i+1)*nrows)
+            coldata = dvals[startidx:endidx]
+            dc[colname] = coldata
+        snametoks  = sname.split(' ')
+        nss = len(snametoks)
+        nrows = len(svals) / nss
+        for i in range(nds):
+            colname = snametoks[i]
+            startidx = int(i*nrows)
+            endidx = int((i+1)*nrows)
+            coldata = svals[startidx:endidx]
+            dc[colname] = coldata
+        
+        # df = pd.DataFrame({dname: dvals, sname: svals})
+        df = pd.DataFrame(dc)
         print(f"On {os.getpid()} received\n{df}")
         # print(f"Received floating data array {request.doublename} with {len(request.doublevalues)} values on OS process {os.getpid()}.")
         # print(f"Received character data array {request.stringname} with {len(request.stringvalues)} values on OS process {os.getpid()}.")
         # Here, you could further process the array as needed
         return handshake_pb2.ArrayResponse(message="Array received successfully!")
 
+    def Aggregate(self, request, context):
+        ports: np.array = np.array(request.ports, dtype=int)
+        for p in ports[1:]:
+            channelName = "localhost:" + str(p)
+            print(f'Thread 0 aggregating its results with {channelName}')
+            channel = grpc.insecure_channel(channelName)
+            stub = handshake_pb2_grpc.HandShakeStub(channel)
+            print(f'Thread 0 aggregating its results with {channelName}')
+            otherThreadResults = stub.GetResults(handshake_pb2.GetResultsRequest(which_results=7))
+            print(f'Python thread 0 received this response from {channelName}')
+            dname: str = otherThreadResults.doublename
+            dvals: np.array = np.array(otherThreadResults.doublevalues, dtype=np.float64)
+            sname: str = otherThreadResults.stringname
+            svals: np.array = np.array(otherThreadResults.stringvalues, dtype=str)
+            df = pd.DataFrame({dname: dvals, sname: svals})
+            print(df)
+        return handshake_pb2.AggregateResponse(return_code=0)
     
+    def GetResults(self, request, context):
+        response = handshake_pb2.StackedTable(doublename="Rocco",
+            doublevalues=[1.1,2.2,3.3],
+            stringname="Sameer", 
+            stringvalues=["IRBB","VAR","Net"])
+        return response
 
 
 def serve(port:int=50051):
