9315425bb409577a7f6a105904310def03fd6ea5
Chris Bailey <baileybunch5dad@gmail.com>
2025-05-22 13:37:25 -0400

    parallel, serial, and recursive

--- cpuserver.py
+++ cpuserver.py
@@ -11,13 +11,19 @@ import numpy as np
 import time
 
 class MyServiceServicer(handshake_pb2_grpc.HandShakeServicer):
-    # Synchronous method
+    # Synchronous method called both synchronously and asynchronously
     def Hello(self, request, context):
-        print("starting hello")
-        print("ending   hello")
-        return handshake_pb2.HelloResponse(reply='fred')
-
-    # Synchronous method called asynchronously
+        print("grpc recursion")
+        stubs = getStubs()
+        print("serial")
+        responses = [stub.GetState(handshake_pb2.EmptyRequest()) for stub in stubs]
+        print("parallel")
+        gsfutures = [stub.GetState.future(handshake_pb2.EmptyRequest()) for stub in stubs]
+        responses = [gsf.result() for gsf in gsfutures]
+        print("ending grp recursion")
+        return handshake_pb2.StateMessage(workerstatus=handshake_pb2.BUILDING_GLOBAL_AGGREGATIONS)
+    
+    # Synchronous method called both synchronously and asynchronously
     def GetState(self, request, context):
         print(f"starting getstate (burning cpu on operating system process {os.getpid()})")
         size = 2 * 10**4
@@ -46,42 +52,35 @@ def tend(t1):
     elapsed = time.perf_counter()-t1
     print(f"Elapsed time {elapsed:.2f}")
 
+def getPorts():
+    return [50051, 50052, 50053, 50054]
+
+def getStubs():
+    return [handshake_pb2_grpc.HandShakeStub(grpc.insecure_channel(f'localhost:{port}')) for port in getPorts()]
+        
+
 if __name__ == "__main__":
-    ports = [50051, 50052, 50053, 50054]
-    processes = []
-    for port in ports:
-        process = multiprocessing.Process(target=serve, args=(port,))
+    processes = [multiprocessing.Process(target=serve, args=(port,)) for port in getPorts()]
+    for process in processes:
         process.start()
-        processes.append(process)
-    stubs = []
 
     time.sleep(2)
-    for port in ports:
-        channel = grpc.insecure_channel(f'localhost:{port}')
-        stub = handshake_pb2_grpc.HandShakeStub(channel)
-        stubs.append(stub)
-    
-    print("Hello")
-    t = tstart()
-    for stub in stubs:
-        resp = stub.Hello(handshake_pb2.HelloRequest(name='fred'))
-    tend(t)
+    stubs = getStubs()
     
     print("Serially burn cpus")
     t = tstart()
-    for stub in stubs:
-        resp = stub.GetState(handshake_pb2.EmptyRequest())
+    responses = [stub.GetState(handshake_pb2.EmptyRequest()) for stub in stubs] 
     tend(t)
     
     
     print(f"Parallel burn cpus")
-    gsfutures = []
-    for stub in stubs:
-        t = tstart()
-        getstate_future = stub.GetState.future(handshake_pb2.EmptyRequest())
-        gsfutures.append(getstate_future)
+    t = tstart()
+    gsfutures = [stub.GetState.future(handshake_pb2.EmptyRequest()) for stub in stubs]
     responses = [gsf.result() for gsf in gsfutures]
     tend(t)
+    
+    print(f"Now from within a method call recursively")
+    stubs[0].Hello(handshake_pb2.HelloRequest(name='test'))
     print("Test completed")
     exit(1)
  
